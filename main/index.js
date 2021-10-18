cb210fc866f33cff6e296cee237f0682d90d7f40
Pao <algumacoisaqns@gmail.com>
2021-10-18 13:17:03 -0300

    Atualizando Configurações

--- main/index.js
+++ main/index.js
@@ -1,13 +1,16 @@
 //cd c:\users\"computer store 04"\desktop\projetos\"-teste-AForum"\main
 
+//Importando e inicializando Express
 const express = require("express");
 const app = express();
 
-
+//Setando princial
 app.get("/",(req,res)=>{
     res.send("Bem Vindo ao AForum!");
 });
 
+
+//Iniciando Servidor
 app.listen(3000,()=>{
     console.log("Servidor Iniciado com Sucesso!");
 });
